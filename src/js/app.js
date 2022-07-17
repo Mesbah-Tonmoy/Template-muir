3d5ccdf3ba72f1197bf5d97d4f674697852e425f
Tonmoy <mesbah@gmail.com>
2022-07-17 06:16:09 +0600

    17/7/2022

--- src/js/app.js
+++ src/js/app.js
@@ -82,16 +82,33 @@ var swiper = new Swiper(".mySwiper7", {
   },
 });
 
+/*==================================*/
 /*            Accordion             */
 /*==================================*/
+
 function accordion(pid, img){
     var pid = document.getElementById(pid);
     var img = document.getElementById(img);
     if (pid.className.indexOf("show") == -1) {
       pid.className += " show";
+      pid.style.display = "block";
       img.src = "assets/media/Close.png";
     } else { 
       pid.className = pid.className.replace(" show", "");
+      pid.style.display = "none";
       img.src = "assets/media/Open.png";
     }
   }
+
+
+  function accordion2(li, text, img_name){
+    var li = document.getElementById(li);
+    var active = document.querySelector('.active');
+    var acc_p = document.getElementById('acc_p');
+    var acc_img = document.getElementById('acc_img');
+
+    active.className = active.className.replace("active", "");
+    li.className += "active";
+    acc_p.innerHTML = text;
+    acc_img.src = 'assets/media/'+img_name;
+}
