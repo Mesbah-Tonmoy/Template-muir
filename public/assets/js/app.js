8510d49e8eee485dfacee48b1ee18d565de145a8
Tonmoy <mesbah@gmail.com>
2022-08-07 07:39:54 +0600

    new update 7/8/2022

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -145,42 +145,6 @@ function muir_swiper(){
 }
 
 /*==================================*/
-/*            Accordion             */
-/*==================================*/
-
-function accordion(pid, img){
-    var pid = document.getElementById(pid);
-    var img = document.getElementById(img);
-    var show = document.querySelector('.show');
-    if (pid.className.indexOf("show") == -1) {
-      pid.className += " show";
-      pid.style.display = "block";
-      img.src = "assets/media/Close.png";
-      show.className = show.className.replace(" show", "");
-      show.style.display = "none";
-      var img_id = show.getAttribute('data-id');
-      document.getElementById(img_id).src = "assets/media/Open.png";
-    } else { 
-      pid.className = pid.className.replace(" show", "");
-      pid.style.display = "none";
-      img.src = "assets/media/Open.png";
-    }
-  }
-
-
-  function accordion2(li, text, img_name){
-    var li = document.getElementById(li);
-    var active = document.querySelector('.active');
-    var acc_p = document.getElementById('acc_p');
-    var acc_img = document.getElementById('acc_img');
-
-    active.className = active.className.replace("active", "");
-    li.className += "active";
-    acc_p.innerHTML = text;
-    acc_img.src = 'assets/media/'+img_name;
-}
-
-/*==================================*/
 /*              Scroll              */
 /*==================================*/
 // window.onload=scroll_to;
@@ -295,3 +259,39 @@ function muir_magnific_popup(){
 
 
 })(jQuery);
+
+/*==================================*/
+/*            Accordion             */
+/*==================================*/
+
+function accordion(pid, img){
+    var pid = document.getElementById(pid);
+    var img = document.getElementById(img);
+    var show = document.querySelector('.show');
+    if (pid.className.indexOf("show") == -1) {
+      pid.className += " show";
+      pid.style.display = "block";
+      img.src = "assets/media/Close.png";
+      show.className = show.className.replace(" show", "");
+      show.style.display = "none";
+      var img_id = show.getAttribute('data-id');
+      document.getElementById(img_id).src = "assets/media/Open.png";
+    } else { 
+      pid.className = pid.className.replace(" show", "");
+      pid.style.display = "none";
+      img.src = "assets/media/Open.png";
+    }
+  }
+
+
+function accordion2(li, text, img_name){
+    var li = document.getElementById(li);
+    var active = document.querySelector('.active');
+    var acc_p = document.getElementById('acc_p');
+    var acc_img = document.getElementById('acc_img');
+
+    active.className = active.className.replace("active", "");
+    li.className += "active";
+    acc_p.innerHTML = text;
+    acc_img.src = 'assets/media/'+img_name;
+}
